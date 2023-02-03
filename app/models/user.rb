c821f29c551442648fbc81939768b3d083b6676f
wakaeri <wwkkeri@gmail.com>
2023-02-03 21:01:05 +0900

    本人情報確認設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,8 @@ class User < ApplicationRecord
   validates :nickname,                presence: true
   # validates :email,                   presence: true
   # validates :password,                presence: true
-  validates :user_name_first,         presence: true
-  validates :user_name_last,          presence: true
-  validates :user_name_first_reading, presence: true
-  validates :user_name_last_reading,  presence: true
+  
+  
   validates :birthday,                presence: true
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -18,5 +16,13 @@ class User < ApplicationRecord
   
   VALID_PASSWORD_REGEX = /\A[a-z\d]{6,}+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-  
+
+  VALID_USER_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  validates :user_name_first,         {presence: true, format: { with: VALID_USER_NAME_REGEX}}
+  validates :user_name_last,          {presence: true, format: { with: VALID_USER_NAME_REGEX}}
+
+  VALID_USER_NAME_READING_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :user_name_first_reading, {presence: true, format: { with: VALID_USER_NAME_READING_REGEX}}
+  validates :user_name_last_reading,  {presence: true, format: { with: VALID_USER_NAME_READING_REGEX}}
+
 end
