efa45630ecf3bbb5423bc38c00688e921f982819
wakaeri <wwkkeri@gmail.com>
2023-02-03 20:17:16 +0900

    ログイン、ユーザー情報設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,                presence: true
-  validates :email,                   presence: true
-  validates :password,                presence: true
+  # validates :email,                   presence: true
+  # validates :password,                presence: true
   validates :user_name_first,         presence: true
   validates :user_name_last,          presence: true
   validates :user_name_first_reading, presence: true
   validates :user_name_last_reading,  presence: true
-  validates :bitthday,                presence: true
+  validates :birthday,                presence: true
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+  
+  VALID_PASSWORD_REGEX = /\A[a-z\d]{6,}+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
   
 end
