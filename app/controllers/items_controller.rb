d46c1cdb0cf470b6b80aa384c69bee288097240a
wakaeri <wwkkeri@gmail.com>
2023-02-04 20:40:48 +0900

    ActiveHash導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,26 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.order("created_at DESC")
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create_table
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
 
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :category_id, :information, :status_id, :delivery_charge_id, :prefecture_id, :days_to_delivery_id, :price).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,19 @@ class Item < ApplicationRecord
   belongs_to :user
   # has_one    :order
   has_one_attached :image
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :days_to_delivery
+  
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :days_to_delivery_id, numericality: { other_than: 1, message: "can't be blank" } 
 
 end
