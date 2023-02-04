1219cd0977bd26af5bbe8a0ec5c1fb43048d0442
wakaeri <wwkkeri@gmail.com>
2023-02-04 20:52:48 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,10 +11,14 @@ class Item < ApplicationRecord
   belongs_to :days_to_delivery
   
   #ジャンルの選択が「---」の時は保存できないようにする
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :status_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :category_id,         numericality: { other_than: 1, message: "can't be blank" } 
+  validates :status_id,           numericality: { other_than: 1, message: "can't be blank" } 
+  validates :delivery_charge_id,  numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefecture_id,       numericality: { other_than: 1, message: "can't be blank" } 
   validates :days_to_delivery_id, numericality: { other_than: 1, message: "can't be blank" } 
-
+  
+  validates :item_name,   presence: true
+  validates :information, presence: true
+  validates :price,       presence: true
+  validates :image,       presence: true
 end
