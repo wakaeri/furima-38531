91611bbcbeb83b1a2a30ce3e9a165a28ffe567b9
wakaeri <wwkkeri@gmail.com>
2023-02-06 19:21:57 +0900

    itemsコントローラーにeditアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.order("created_at DESC")
@@ -22,6 +22,8 @@ class ItemsController < ApplicationController
   def show
   end
 
+  def edit
+  end
 
   private
 
