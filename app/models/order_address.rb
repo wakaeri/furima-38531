181ec1a57b33dd28adfa780d97711af356d95a65
wakaeri <wwkkeri@gmail.com>
2023-02-12 17:11:16 +0900

    売却済み商品条件分岐実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -15,10 +15,7 @@ class OrderAddress
 
 
   def save
-    # 商品購入情報を保存し、変数orderに代入する
     order = Order.create(item_id: item_id, user_id: user_id)
-    # 住所を保存する
-    # order_idには、変数orderのidと指定する
     DeliveryAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
