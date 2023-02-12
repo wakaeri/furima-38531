4b194443017f3db03bd4a1ea03bba09c74b80f96
wakaeri <wwkkeri@gmail.com>
2023-02-12 18:43:26 +0900

    修正3_電話番号バリデーション正規表現_可読性

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,7 +9,7 @@ class OrderAddress
     validates :prefecture_id,       numericality: { other_than: 0, message: "can't be blank" } 
     validates :city
     validates :house_number
-    validates :phone_number, format: {with: /\A[0-9]\d{9,10}\z/, message: "is invalid"}
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid"}
     validates :token
   end
 
